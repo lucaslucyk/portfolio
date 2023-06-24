1604624e752a905b03e316170e57f09932d707bf
Lucas Lucyk <llucyk@grupospec.com>
2023-06-24 01:29:57 -0300

    add some projects

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Icon } from "@iconify/react";
-import angularIcon from "@iconify/icons-logos/angular-icon";
-import reactIcon from "@iconify/icons-logos/react";
-import vueIcon from "@iconify/icons-logos/vue";
+import angularIcon from "@iconify/icons-logos/python";
+import reactIcon from "@iconify/icons-logos/kubernetes";
+import vueIcon from "@iconify/icons-logos/django-icon";
 
 class About extends Component {
   render() {
