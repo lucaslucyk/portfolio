6bddabbc4a06abe48a0019ea8967ccec21f2c4ee
Lucas Lucyk <llucyk@grupospec.com>
2023-06-27 15:50:28 -0300

    Updates

---
+++ precache-manifest.6ea3df1b7bcafb14dfa94dbd791be96d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b6fe118faaca8579a2727649295912",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a9977c9778f086ccd0be",
+    "url": "/portfolio/static/css/2.3eb2a443.chunk.css"
+  },
+  {
+    "revision": "04d8548889565fab0518",
+    "url": "/portfolio/static/css/main.119f8949.chunk.css"
+  },
+  {
+    "revision": "a9977c9778f086ccd0be",
+    "url": "/portfolio/static/js/2.77d3d433.chunk.js"
+  },
+  {
+    "revision": "334a3166bc9976dda67650e8c6f88652",
+    "url": "/portfolio/static/js/2.77d3d433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04d8548889565fab0518",
+    "url": "/portfolio/static/js/main.ae627c05.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  }
+]);
